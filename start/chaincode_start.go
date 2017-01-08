02044e06a948745f148dc4cf5c13f58e383c82be
zduniak <zduniak@gmail.com>
2017-01-08 18:52:49 +0100

    initial changes

--- start/chaincode_start.go
+++ start/chaincode_start.go
@@ -42,6 +42,11 @@ func (t *SimpleChaincode) Init(stub shim.ChaincodeStubInterface, function string
 	if len(args) != 1 {
 		return nil, errors.New("Incorrect number of arguments. Expecting 1")
 	}
+	
+	err := stub.PutState("hello_world", []byte()args[0]));
+	if err != nil {
+		return nil, err
+	}
 
 	return nil, nil
 }
@@ -53,10 +58,31 @@ func (t *SimpleChaincode) Invoke(stub shim.ChaincodeStubInterface, function stri
 	// Handle different functions
 	if function == "init" {													//initialize the chaincode state, used as reset
 		return t.Init(stub, "init", args)
+	} else if function == "write" {
+		return t.write(stub, args)
 	}
 	fmt.Println("invoke did not find func: " + function)					//error
 
 	return nil, errors.New("Received unknown function invocation: " + function)
+}
+
+func (t *SimpleChaincode) write(stub shim.ChaincodeStubInterface, args []string) ([]byte, error) {
+	var name, value string
+	var err error
+	fmt.Println("running write()")
+	
+	if len(args) != 2 {
+		return nul, errors.New("Incorrect number of arguments. Expecting 2. name of the variable and value to set")
+	}
+	
+	name = args[0]
+	value = args[1]
+	err = stub.PutState(name, []byte(value))
+	if err != nil {
+		return nil, err
+	}
+	
+	return nil, nil
 }
 
 // Query is our entry point for queries
@@ -64,11 +90,29 @@ func (t *SimpleChaincode) Query(stub shim.ChaincodeStubInterface, function strin
 	fmt.Println("query is running " + function)
 
 	// Handle different functions
-	if function == "dummy_query" {											//read a variable
-		fmt.Println("hi there " + function)						//error
-		return nil, nil;
+	if function == "read" {
+		return t.read(stub, args);
 	}
 	fmt.Println("query did not find func: " + function)						//error
 
 	return nil, errors.New("Received unknown function query: " + function)
+}
+
+
+func (t *SimpleChaincode) read(stub shim.ChaincodeStubInterface, args []string) ([]byte, error) {
+	var name, jsonResp string
+	var err error
+	
+	if len(args) != 1 {
+		return nil, errors.New("Incorrect number of arguments. Expecting name of the var to query")
+	}
+	
+	name = args[0]
+	valAsbytes, err = stub.GetState(name)
+	if err != nil {
+		jsonResp = "{\"Error\":\"Failed to get state for " + name + "\"}"
+		return nil, errors.New(jsonResp)
+	}
+	
+	return valAsbytes, nil
 }
